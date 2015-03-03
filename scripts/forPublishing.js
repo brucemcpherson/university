b9fa6a5ba6b9abdd2d2891cec3a5be53f4865d8e
Bruce McPherson <bruce@mcpher.com>
2015-03-03 12:36:08 -0500

    updated by GasGit automation

--- scripts/forPublishing.js
+++ scripts/forPublishing.js
@@ -3,8 +3,9 @@ function showMyScriptDb() {
   return publicStuffDb();
 }
 function publicStuffDb() {
-  return ScriptDb.getMyDb();
+  return getDefaultDB();
 }
+
 function showMyScriptAppResource(s) {
   try {
     return ScriptApp.getResource(s);
@@ -12,4 +13,12 @@ function showMyScriptAppResource(s) {
   catch (err) {
     throw err + " getting script " + s;
   }
+}
+function getDefaultDB (optSilo) {
+
+  return new cRipDB.RipDB (new cDbAbstraction.DbAbstraction ( cDriverMongoLab , {
+    siloid:optSilo || 'university',
+    dbid:'xliberation',
+    driverob:JSON.parse(PropertiesService.getScriptProperties().getProperty("mongoLabKeys"))
+  }));
 }
